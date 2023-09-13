c874689c50cc6d3922abd0b9057449890dbe645d
Wander Amaral <wanderamaral013@gmail.com>
2023-09-13 11:42:45 -0300

    commit

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,23 +1,45 @@
 import { useState } from "react";
-
+import { useSelector, useDispatch } from "react-redux";
 // Components
 import Cart from "../cart/index";
+import UserActionTypes from "../../redux/user/action-types";
 
 // Styles
 import * as Styles from "./styles";
 
+
+
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
+  const {currentUser} = useSelector((rootReducer) => rootReducer.userReducer)
+  const dispacht = useDispatch()
+
+  console.log({currentUser})
+
   const handleCartClick = () => {
     setCartIsVisible(true);
+  };
+
+  const handleLoginClick = () => {
+      dispacht({
+        type: UserActionTypes.LOGIN,
+        payload: {name: "Wander", email:"wander@amaral.com"}
+      });
+  };
+
+  const handleLogoutClick = () => {
+    dispacht({
+      type: UserActionTypes.LOGOUT,
+    });
   };
 
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
+        {currentUser ? (<div onClick={handleLogoutClick}>Sair</div>) : (<div onClick={handleLoginClick}>Login</div>)}
+        
         <div onClick={handleCartClick}>Carrinho</div>
       </Styles.Buttons>
 
